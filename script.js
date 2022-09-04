bb01b2994af60122d2c30512dd272ddfbe7c5126
reshma <reshmashaik0910@gmail.com>
2022-09-04 19:37:38 +0100

    Resolved Bug

--- script.js
+++ script.js
@@ -58,16 +58,7 @@ function game(){
         }
         else{
             alert("Wrong input,choose correct");
-            playerSelection = prompt("Please choose Rock or Paper or Scissors").toLowerCase();
-            if(playerSelection ==="paper" || playerSelection ==="rock" || playerSelection ==="scissors"){
-                computerSelection = computerPlay();
-                console.log(playRound(playerSelection, computerSelection));
-            }
-            else{
-                alert("Wrong input,choose correct");
-                i--;
-            }
-            
+            i--;
         }
     }   
     if(playerScore > computerScore){
